fc4828b786596ea159605e1b441eeff1ce89f44d
gonzam77 <gonzam77@gmial.com>
2023-04-21 14:14:29 -0300

    landing button responsive

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -9,7 +9,7 @@ export default function Landing() {
     return (
         <div className={styles.container}>
             <h1>Welcome to Videogames!</h1>
-            <div>
+            <div className={styles.landing}>
                 <Link to="/home">
                     <button className={styles.button}>ENTER</button>
                 </Link>
